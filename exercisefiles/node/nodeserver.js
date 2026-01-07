b53d3759acbe4a7ad5c0b16882b8763cd9fca9eb
robpitcher <robpitcher@microsoft.com>
2026-01-07 10:27:56 -0500

    Implement basic Node.js server with GET method handling

--- exercisefiles/node/nodeserver.js
+++ exercisefiles/node/nodeserver.js
@@ -5,3 +5,30 @@
 // if the url has other methods, return "method not supported"
 // when server is listening, log "server is listening on port 3000"
 
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
+    if (pathname === '/get') {
+        const key = query.key;
+        if (key) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('hello ' + key);
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('key not passed');
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('method not supported');
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
